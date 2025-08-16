e20204520e2924a9ff87f5ba7b0a25a2fac5ee8f
surbhit <surbhit.thakur@gmail.com>
2025-08-16 13:13:28 +0530

    feat: add validation for phonenumber and email

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,26 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ "message": "access /identify" })
 })
 
+// Basic validaton check for phone and email
+const isValidBody = (phoneNumber: string, email: string): boolean => {
+    const phoneRegex = /^\d{10}$/;
+    const isPhoneValid = phoneRegex.test(phoneNumber);
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isEmailValid = emailRegex.test(email);
+
+    return isPhoneValid && isEmailValid;
+}
+
 app.get('/identify', async (req: Request, res: Response) => {
-    if (!req.body) res.status(400).json({'message': 'require atleast phoneNumber or email in req.body'})
+    if (!req.body) res.status(400).json({ 'message': 'require atleast phoneNumber or email in req.body' })
+
     let { phoneNumber, email } = req.body
-    let rows = await identify(phoneNumber, email)
-    console.log(rows)
-    res.status(200).json(rows)
+    if (!isValidBody(phoneNumber, email)) res.status(400).json({ 'message': 'invalid phoneNumber(10 digits required) or email(abc@def.com format required)' })
+
+    let consolidated = await identify(phoneNumber, email)
+
+    res.status(200).json(consolidated)
 })
 
 export default app
