8461f1cc4677fa55cd8b4f0324ff1d950feed4e0
surbhit <surbhit.thakur@gmail.com>
2025-08-16 11:04:31 +0530

    chore: formatting

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import express from 'express'
 import { identify } from './controllers/orderController'
 
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(express.json())
 
-app.get( '/', (req: Request, res: Response) => {
-    res.status(200).json({"message" : "access /identify"})
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ "message": "access /identify" })
 })
 
-app.get( '/identify', async (req: Request, res: Response) => {
-    let {phoneNumber, email} = req.body
+app.get('/identify', async (req: Request, res: Response) => {
+    let { phoneNumber, email } = req.body
     let rows = await identify(phoneNumber, email)
     console.log(rows)
     res.status(200).json(req.body)
